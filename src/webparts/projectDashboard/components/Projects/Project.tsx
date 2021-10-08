eecb3b619e8250bce1c0662e02760bf16f0947d8
André Luiz <an3dree@gmail.com>
2021-10-08 08:59:12 -0300

    add project webpart

--- src/webparts/projectDashboard/components/Projects/Project.tsx
+++ src/webparts/projectDashboard/components/Projects/Project.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
-import {ProjectsState} from './ProjectsState'
-import {ProjectsProps} from './ProjectsProps'
+import {ProjectsState} from './ProjectsState';
+import {ProjectsProps} from './ProjectsProps';
+import {
+    SPHttpClient,
+    SPHttpClientResponse,
+    SPHttpClientCommonConfiguration,
+    SPHttpClientConfiguration
+} from '@microsoft/sp-http';
 
 export class Projects extends React.Component<ProjectsProps, ProjectsState>{
 
@@ -12,7 +18,23 @@ export class Projects extends React.Component<ProjectsProps, ProjectsState>{
     }
 
     public getItems(){
-
+        console.log('em cima do this')
+        this.props.context.spHttpClient
+        .get(
+            `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/GetByTitle('Projects')/Items?$expand=ProjectManager&$select=*,ProjectManager,ProjectManager/EMail,ProjectManager/Title`,
+            SPHttpClient.configurations.v1
+        ).then(
+            (response: SPHttpClientResponse): Promise<{ value: any[]}> => {
+                return response.json();
+                console.log('dentro do then')
+            }
+        ).then((response: { value: any[] }) => {
+            var _items = [];
+            _items = _items.concat(response.value);
+            this.setState({
+                items: _items,
+            });
+        });
     }
 
     public componentDidMount(){
@@ -22,7 +44,11 @@ export class Projects extends React.Component<ProjectsProps, ProjectsState>{
     public render(): React.ReactElement<ProjectsProps>{
         return(
             <div>
-                Hello from Projects Component
+                {this.state.items.map((item, key) => {
+                    <li key={key}>
+                        <h3>{item.Title}</h3>
+                    </li>
+                })}
             </div>
         );
     }
